b3b941e17c216eedd4e8ab456d5ddb2ee6cf9dcd
Aldair MH <aldairmh@outlook.es>
2021-08-15 06:35:15 -0500

    environment

--- users/controller/usersController.py
+++ users/controller/usersController.py
@@ -1,7 +1,12 @@
-from flask import Blueprint
+from flask import Blueprint, request, jsonify
 
 users_blueprint = Blueprint('users', __name__)
 
 @users_blueprint.route('/users', methods=['GET'])
 def get_users():
     return 'Getting users'
+
+@users_blueprint.route('/users', methods=['POST'])
+def create_user():
+    data = request.json
+    return jsonify(data), 200
